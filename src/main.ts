a750cceb4c2b7c3c2f9ae18f546c5d1b551f1715
Badriddin Karimjonov <b.karimjonov@uzautotrailer.local>
2026-05-12 12:05:31 +0500

    setup backend auto deploy

--- src/main.ts
+++ src/main.ts
@@ -5,21 +5,60 @@ import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
-  app.use(helmet({
-    crossOriginResourcePolicy: false, // Rasmlar ko'rinishi uchun bu false bo'lishi shart
-  }));
 
-  // 1. CORS ni serverga moslash
+  app.use(
+    helmet({
+      crossOriginResourcePolicy: false,
+    }),
+  );
+
   app.enableCors({
-    origin: '*', // Hozircha hamma, lekin serverga chiqqanda faqat frontend domeningizni yozasiz
+    origin: [
+      'https://uzautotrailer.uz',
+      'https://www.uzautotrailer.uz',
+      'https://api.uzautotrailer.uz',
+      'https://admin.uzautotrailer.uz', // <--- Buni qo'shing
+      'http://admin.uzautotrailer.uz',  // <--- SSL o'rniguncha bu ham kerak
+      'http://localhost:5173',
+      'http://localhost:3000',
+      'https://test.uzautotrailer.uz', // <--- Buni qo'shing
+      'http://test.uzautotrailer.uz',  // <--- SSL o'rniguncha bu ham bo'lsin
+      'https://uzauto-front.vercel.app',
+      'https://uzauto-front-i8t22gw1j-sherzodbekwebs-projects.vercel.app',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
 
-  // 2. Portni server dinamik aniqlashi uchun process.env qo'shish
-  const port = process.env.PORT || 3001;
+  const port = process.env.PORT || 3000;
   await app.listen(port);
+
   console.log(`Backend is running on port: ${port}`);
 }
 bootstrap();
+
+
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import helmet from 'helmet';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+
+//   app.use(helmet({
+//     crossOriginResourcePolicy: false, // Rasmlar ko'rinishi uchun bu false bo'lishi shart
+//   }));
+
+//   // 1. CORS ni serverga moslash
+//   app.enableCors({
+//     origin: '*', // Hozircha hamma, lekin serverga chiqqanda faqat frontend domeningizni yozasiz
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//     credentials: true,
+//   });
+
+//   // 2. Portni server dinamik aniqlashi uchun process.env qo'shish
+//   const port = process.env.PORT || 3001;
+//   await app.listen(port);
+//   console.log(`Backend is running on port: ${port}`);
+// }
+// bootstrap();
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -12,7 +12,7 @@ describe('AppController (e2e)', () => {
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = moduleFixture.createNestApplication(); 
     await app.init();
   });
 
