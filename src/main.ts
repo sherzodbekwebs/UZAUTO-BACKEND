e87ee755c8a2fcb5339f17d18bdb398cd77e4f4b
Badriddin Karimjonov <b.karimjonov@uzautotrailer.local>
2026-05-12 14:29:19 +0500

    dsf

--- src/main.ts
+++ src/main.ts
@@ -26,7 +26,7 @@ async function bootstrap() {
       'https://uzauto-front.vercel.app',
       'https://uzauto-front-i8t22gw1j-sherzodbekwebs-projects.vercel.app',
     ],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
 
@@ -36,29 +36,3 @@ async function bootstrap() {
   console.log(`Backend is running on port: ${port}`);
 }
 bootstrap();
-
-
-// import { NestFactory } from '@nestjs/core';
-// import { AppModule } from './app.module';
-// import helmet from 'helmet';
-
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-
-//   app.use(helmet({
-//     crossOriginResourcePolicy: false, // Rasmlar ko'rinishi uchun bu false bo'lishi shart
-//   }));
-
-//   // 1. CORS ni serverga moslash
-//   app.enableCors({
-//     origin: '*', // Hozircha hamma, lekin serverga chiqqanda faqat frontend domeningizni yozasiz
-//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//     credentials: true,
-//   });
-
-//   // 2. Portni server dinamik aniqlashi uchun process.env qo'shish
-//   const port = process.env.PORT || 3001;
-//   await app.listen(port);
-//   console.log(`Backend is running on port: ${port}`);
-// }
-// bootstrap();
